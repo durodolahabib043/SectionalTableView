f07a329393c1212141d749ce302c25473ad5d65d
habib <durodolahabib043@gmail.com>
2020-03-01 09:46:16 -0500

    first commit

---
+++ SectionalContactTableview/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SectionalContactTableview
+//
+//  Created by Habib Durodola on 2020-03-01.
+//  Copyright © 2020 doh. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ SectionalContactTableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SectionalContactTableview
+//
+//  Created by Habib Durodola on 2020-03-01.
+//  Copyright © 2020 doh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
